1b360ea42e27c483296ec18504c9d661f256be65
Robert Laws <roblaws@me.com>
2019-06-04 05:43:25 +0000

    Automatically backed up by Learn

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,30 +1,4 @@
-class User < ApplicationRecord
-  has_secure_password
-
+class Attraction < ApplicationRecord
   has_many :rides
-  has_many :attractions, through: :rides
-
-  validates :name,
-    presence: true,
-    uniqueness: true,
-    on: :create
-
-  validates :password,
-    presence: true,
-    on: :create
-
-  def mood
-    # if the user is more nauseous than happy
-    mood = "okay"
-    if happiness > nausea
-      mood = "happy"
-    elsif nausea > happiness
-      mood = "sad"
-    end
-    mood
-  end
-
-  def user_type
-    self.admin ? "ADMIN" : "USER"
-  end
+  has_many :users, through: :rides
 end
